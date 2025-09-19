ea29738abe5faffcf911f3aba1f7856df35c0d43
Li-yao Xia <lysxia@gmail.com>
2025-09-19 17:58:24 +0200

    wip

--- src/intrinsics/mod.rs
+++ src/intrinsics/mod.rs
@@ -1,41 +1,41 @@
 #![allow(unused_variables)]
 
 pub const unsafe fn exact_div<T: Copy>(x: T, y: T) -> T {
-    todo!("intrinsics")
+    panic!("intrinsics")
 }
 
 pub const unsafe fn cttz_nonzero<T: Copy>(x: T) -> u32 {
-    todo!("intrinsics")
+    panic!("intrinsics")
 }
 
 pub const fn mul_with_overflow<T: Copy>(x: T, y: T) -> (T, bool) {
-    todo!("intrinsics")
+    panic!("intrinsics")
 }
 
 pub const unsafe fn unchecked_rem<T: Copy>(x: T, y: T) -> T {
-    todo!("intrinsics")
+    panic!("intrinsics")
 }
 
 pub const unsafe fn unchecked_shl<T: Copy, U: Copy>(x: T, y: U) -> T {
-    todo!("intrinsics")
+    panic!("intrinsics")
 }
 
 pub const unsafe fn unchecked_shr<T: Copy, U: Copy>(x: T, y: U) -> T {
-    todo!("intrinsics")
+    panic!("intrinsics")
 }
 
 pub const unsafe fn unchecked_sub<T: Copy>(x: T, y: T) -> T {
-    todo!("intrinsics")
+    panic!("intrinsics")
 }
 
 pub const fn wrapping_add<T: Copy>(a: T, b: T) -> T {
-    todo!("intrinsics")
+    panic!("intrinsics")
 }
 
 pub const fn wrapping_mul<T: Copy>(a: T, b: T) -> T {
-    todo!("intrinsics")
+    panic!("intrinsics")
 }
 
 pub const fn wrapping_sub<T: Copy>(a: T, b: T) -> T {
-    todo!("intrinsics")
+    panic!("intrinsics")
 }
